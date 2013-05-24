25bdcc8ca269b5326f193ad9eb5cd2d736e57fdf
Aaron Thornton <aaronthornton00@gmail.com>
2013-05-24 20:26:18 +1200

    try this on for size

--- spec/models/motion_spec.rb
+++ spec/models/motion_spec.rb
@@ -18,7 +18,7 @@ describe Motion do
     time_zone = "NZST"
     motion = build(:motion, close_at_date: close_date, close_at_time: close_time, close_at_time_zone: time_zone)
     motion.save!
-    motion.close_at.should == current_time.strftime('%Y-%m-%d %H:00 %Z')
+    Time.parse(motion.close_at.strftime('%Y-%m-%d %H:00 %Z')).utc.should == Time.parse(current_time.strftime('%Y-%m-%d %H:00 %Z')).utc
   end
 
   describe "#user_has_voted?(user)" do
