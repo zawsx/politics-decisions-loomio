f77bec43eddd3f1adaf130bda21b95ac64286827
Aaron Thornton <aaronthornton00@gmail.com>
2013-07-03 17:05:37 +1200

    fix signed out forms

--- app/controllers/group_requests_controller.rb
+++ app/controllers/group_requests_controller.rb
@@ -1,5 +1,5 @@
 class GroupRequestsController < BaseController
-  before_filter :authenticate_user!, except: [:verify, :new, :create, :confirmation]
+  before_filter :authenticate_user!, except: [:verify, :new, :create, :confirmation, :group_sign_up_formal, :group_sign_up_informal]
   before_filter :already_verified, only: :verify
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Loomio::Application.routes.draw do
     get :verify, on: :member
     get :group_sign_up_informal, on: :member
     get :group_sign_up_formal, on: :member
+    get :sign_up_confirmation, on: :member
   end
 
   match "/request_new_group", to: "group_requests#new", as: :request_new_group
