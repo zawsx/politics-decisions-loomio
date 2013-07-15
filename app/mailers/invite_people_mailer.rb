2e478009e434f42a113fb4e0e00fa69df1fd8d6d
Jesse Doud <jessedoud@gmail.com>
2013-07-15 16:17:25 +1200

    change subject in email
    Conflicts:
    	app/mailers/invite_people_mailer.rb
    
    Conflicts:
    	app/mailers/invite_people_mailer.rb

--- app/mailers/invite_people_mailer.rb
+++ app/mailers/invite_people_mailer.rb
@@ -2,7 +2,7 @@ class InvitePeopleMailer < BaseMailer
   def to_start_group(invitation, sender_email)
     @invitation = invitation
     mail to: invitation.recipient_email,
-         from: 'contact@loomio.org',
+         from: 'Loomio <contact@loomio.org>',
          reply_to: sender_email,
          subject: "Your Loomio group: #{@invitation.group_name} has been approved!"
   end
@@ -11,7 +11,7 @@ class InvitePeopleMailer < BaseMailer
     @invitation = invitation
     @message_body = message_body
     mail to: invitation.recipient_email,
-         from: 'contact@loomio.org',
+         from: 'Loomio <contact@loomio.org>',
          reply_to: sender_email,
          subject: "#{@invitation.inviter.name} has invited you to join #{@invitation.group_name} on Loomio"
   end
@@ -21,7 +21,7 @@ class InvitePeopleMailer < BaseMailer
     @group = @invitation.group
     @message_body = message_body
     mail to: invitation.recipient_email,
-         from: 'contact@loomio.org',
+         from: 'Loomio <contact@loomio.org>',
          reply_to: sender_email,
          subject: "#{email_subject_prefix(@group.full_name)} " + t("email.group_membership_approved.subject")
   end
