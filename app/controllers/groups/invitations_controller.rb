cb6d2a628659048ec7cdaedb10a0646d9250790a
john irving <whimful@gmail.com>
2013-08-07 14:18:54 +1200

    wip

--- app/controllers/groups/invitations_controller.rb
+++ app/controllers/groups/invitations_controller.rb
@@ -8,28 +8,28 @@ class Groups::InvitationsController < GroupBaseController
   end
 
   def create
-
     emails = params[:invite_people][:recipients].split(',')
     existing_users = User.where(email: emails)
     existing_emails = existing_users.pluck(:email)
+    already_in_group = existing_users.where
+    existing_users_not_in_group = existing_users - @group.members.all
     new_emails = emails - existing_emails
 
     existing_users.each do |user|
       @group.add_member!(user)
     end
+    @num_auto_added = existing_users.count
 
 
     @invite_people = InvitePeople.new(params[:invite_people])
-    @invite_people.recipients = new_emails.join
-    p "000000000000000000000000000000000000"
-    p @invite_people
-    p "000000000000000000000000000000000000"
+    @invite_people.recipients = new_emails.join(',')
+
     if @invite_people.valid?
-      num = CreateInvitation.to_people_and_email_them(@invite_people, group: @group, inviter: current_user)
-      flash[:notice] = "#{num} invitation(s) sent" if num > 0
+      @num_invited = CreateInvitation.to_people_and_email_them(@invite_people, group: @group, inviter: current_user)
+      set_invitation_success_flash
       redirect_to group_path(@group)
-    elsif @invite_people.recipients.empty?
-      flash[:notice] = "yussss"
+    elsif @num_auto_added > 0
+      set_invitation_success_flash
       redirect_to group_path(@group)
     else
       load_decorated_group
@@ -62,5 +62,18 @@ class Groups::InvitationsController < GroupBaseController
 
   def load_decorated_group
     @group = GroupDecorator.new(Group.find(params[:group_id]))
+  end
+
+  def set_invitation_success_flash
+    if @num_auto_added == 0 && @num_invited > 0
+      flash[:notice] = t(:'notice.invitations.sent', count: @num_invited)
+    elsif @num_auto_added > 0 && @num_invited == 0
+      flash[:notice] = t(:'notice.invitations.auto_added', count: @num_auto_added)
+    else
+      flash[:notice] = t(:'notice.invitations.auto_added_and_sent',
+                            auto_added_text: t(:'notice.invitations.auto_added', count: @num_auto_added),
+                            sent_text: t(:'notice.invitations.sent', count: @num_invited))
+      # flash[:notice] = t(:'notice.invitations.auto_added_and_sent', num_added: @num_auto_added, num_invited: @num_invited)
+    end
   end
 end
