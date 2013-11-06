31cedafecd654b34b5c122256ac0c53b3f93667d
Jesse Doud <jessedoud@gmail.com>
2013-11-06 16:43:05 +1300

    wip

--- features/motions/proposal_outcome.feature
+++ features/motions/proposal_outcome.feature
@@ -1,23 +1,27 @@
 Feature: Coordinator creates proposal outcome
-  As a Loomio group coordinator
-  So I can communicate to my group the results of a proposal
-  I want to be able to create a proposal outcome
+  As a proposal author
+  I want to share the outcome of the proposal with other group members
+  So that the group is informed of the outcome of a proposal and members can act on the decisions
 
   Background:
     Given I am logged in
     And I am an admin of a group
     And there is a discussion in the group
-    And the discussion has a closed proposal
+    And the discussion has a proposal
 
+  @javascript
   Scenario: Coordinator creates a proposal outcome
+    Given I close the proposal
     And I have recieved an email with subject "Proposal closed"
-    When I click the link to create a proposal
-    And I specify a proposal
+    When I click the link to create a proposal outcome
+#    And I see the proposal outcome field highlighted
+    And I specify a proposal outcome
     And I click "Publish outcome"
     Then my group members should receive an email with subject "Proposal outcome"
 
   Scenario: Coordinator edits a proposal outcome
+    Given I close the proposal
     And I have created a proposal outcome
-    When I edit the propsosal outcome
+    When I edit the proposal outcome
     And I click "Save"
     Then my group members should not receive an email with subject "Proposal outcome"
--- features/step_definitions/proposal_outcome_steps.rb
+++ features/step_definitions/proposal_outcome_steps.rb
@@ -1,31 +1,52 @@
-Given(/^the discussion has a closed proposal$/) do
-  pending # express the regexp above with the code you wish you had
+Given(/^the discussion has a proposal$/) do
+  @motion = FactoryGirl.create :motion, discussion: @discussion, author: @user
+  @group_member = FactoryGirl.create :user
+  @group.add_member!(@group_member)
+end
+
+Given(/^I close the proposal$/) do
+  visit discussion_path(@discussion)
+  find('#close-voting').click
+  find('#confirm-action').click
 end
 
 Given(/^I have recieved an email with subject "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  last_email = ActionMailer::Base.deliveries.last
+  last_email.subject.should include arg1
 end
 
-When(/^I click the link to create a proposal$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^I click the link to create a proposal outcome$/) do
+  open_email(@user.email, :with_subject => "Proposal closed")
+  link = links_in_email(current_email)[2]
+  request_uri = URI::parse(link).request_uri
+  visit request_uri
+  view_screenshot
 end
 
-When(/^I specify a proposal$/) do
-  pending # express the regexp above with the code you wish you had
+# When(/^I see the proposal outcome field highlighted$/) do
+#   page.should have_css "#outcome-input:focus"
+# end
+
+When(/^I specify a proposal outcome$/) do
+  fill_in 'motion[outcome]', with: "Let's have an intervention about Rob's odooblem!"
 end
 
 Then(/^my group members should receive an email with subject "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  last_email = ActionMailer::Base.deliveries.last
+  last_email.subject.should =~ /arg1/
 end
 
 Given(/^I have created a proposal outcome$/) do
-  pending # express the regexp above with the code you wish you had
+  @motion.outcome = "Let's have an intervention about Rob's odour Roblem!"
+  find('#edit-outcome').click
 end
 
-When(/^I edit the propsosal outcome$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^I edit the proposal outcome$/) do
+  fill_in '#outcome-input', with: "Let's have an intervention about Rob's odooblem!"
+  click_on 'proposal-submit'
 end
 
 Then(/^my group members should not receive an email with subject "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  last_email = ActionMailer::Base.deliveries.last
+  last_email.subject.should_not =~ /arg1/
 end
