4fa58fd2b2f99fc4e6045d996ae9d33343f89d63
john irving <whimful@gmail.com>
2014-03-20 12:07:40 +1300

    try fix

--- config/initializers/I18n_viz.rb
+++ config/initializers/I18n_viz.rb
@@ -6,5 +6,7 @@ unless defined?(I18nViz).nil?
   # Link to display in the I18nViz tooltip
   # e.g. pointing to that particular string in your apps translation tool (webtranslateit.com, localeapp.com, ...)
   # the i18n key will be appended to this URL
-  I18nViz.external_tool_url = "https://webtranslateit.com/en/projects/1234567/locales/en..de/strings?utf8=✓&s="
+  # I18nViz.external_tool_url = "https://webtranslateit.com/en/projects/1234567/locales/en..de/strings?utf8=✓&s="
+  target_language = "pt_BR"
+  I18nViz.external_tool_url = "https://www.transifex.com/projects/p/loomio-1/translate/#pt_BR/$?key="
 end
